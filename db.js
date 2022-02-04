76baaba9637d7a3dc37b1a986cfe97be2e9f4832
Micaella Morales <morales.micaella@gmail.com>
2022-02-03 21:19:31 -0800

    Add dotenv and change key name

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 exports.connect = () => {
-  // const uri = "mongodb://localhost:27017/todolistDB"
   const uri = "mongodb+srv://cluster0.bk9na.mongodb.net/todolistDB"
   const options = {
-    user: process.env.USER,
-    pass: process.env.PASS,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
     authSource: "admin"
   };
 
